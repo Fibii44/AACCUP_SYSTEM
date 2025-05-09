965bc9446a25039666f14ce0503d83f2c1ed4e7c
JamesRioAbaquita <2201103767@student.buksu.edu.ph>
2025-05-09 12:30:16 +0800

    Commit to test release

--- routes/tenant.php
+++ routes/tenant.php
@@ -21,7 +21,7 @@ use App\Http\Middleware\CheckUserStatus;
 use App\Http\Controllers\Tenant\InstrumentController;
 use App\Http\Controllers\Tenant\UploadController;
 use App\Http\Controllers\Tenant\ProfileController;
-
+use App\Http\Controllers\Tenant\VersionTestingController;
 
 
 /*
@@ -157,5 +157,8 @@ Route::middleware([
         // Upload routes
         Route::get('/indicators/{indicator}/uploads', [UploadController::class, 'index'])->name('tenant.uploads.index');
         Route::post('/indicators/{indicator}/uploads', [UploadController::class, 'store'])->name('tenant.uploads.store');
+
+        // Version Testing routes
+        Route::get('/version-testing', [VersionTestingController::class, 'index'])->name('tenant.version-testing.index');
     });
 });
