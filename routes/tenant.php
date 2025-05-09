6d3c957592e842355391c97cc47a1953783ac137
JamesRioAbaquita <2201103767@student.buksu.edu.ph>
2025-05-09 12:03:50 +0800

    Version Testing

--- routes/tenant.php
+++ routes/tenant.php
@@ -21,6 +21,7 @@ use App\Http\Middleware\CheckUserStatus;
 use App\Http\Controllers\Tenant\InstrumentController;
 use App\Http\Controllers\Tenant\UploadController;
 use App\Http\Controllers\Tenant\ProfileController;
+use App\Http\Controllers\Tenant\VersionTestingController;
 
 
 
@@ -158,5 +159,9 @@ Route::middleware([
         // Upload routes
         Route::get('/indicators/{indicator}/uploads', [UploadController::class, 'index'])->name('tenant.uploads.index');
         Route::post('/indicators/{indicator}/uploads', [UploadController::class, 'store'])->name('tenant.uploads.store');
+
+        // Version Testing routes
+        Route::get('/version-testing', [VersionTestingController::class, 'index'])->name('tenant.version-testing.index');
+        Route::post('/version-testing/test', [VersionTestingController::class, 'test'])->name('tenant.version-testing.test');
     });
 });
