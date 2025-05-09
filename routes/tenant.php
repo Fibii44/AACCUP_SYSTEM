025d7ba2727e14625a61afeaf55210e947ca0597
JamesRioAbaquita <2201103767@student.buksu.edu.ph>
2025-05-09 16:28:03 +0800

    Added Test

--- routes/tenant.php
+++ routes/tenant.php
@@ -21,6 +21,7 @@ use App\Http\Middleware\CheckUserStatus;
 use App\Http\Controllers\Tenant\InstrumentController;
 use App\Http\Controllers\Tenant\UploadController;
 use App\Http\Controllers\Tenant\ProfileController;
+use App\Http\Controllers\Tenant\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -156,7 +157,9 @@ Route::middleware([
         Route::get('/indicators/{indicator}/uploads', [UploadController::class, 'index'])->name('tenant.uploads.index');
         Route::post('/indicators/{indicator}/uploads', [UploadController::class, 'store'])->name('tenant.uploads.store');
        
-        
+
+        Route::get('/test', [TestController::class, 'index'])->name('tenant.test.index');
+
     });
 });
 
